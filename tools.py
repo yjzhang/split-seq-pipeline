9eb7dbdbf0419c5d79b72b594be27b064caa4739
Ubuntu <ubuntu@ip-172-31-47-168.us-west-2.compute.internal>
2018-11-20 02:44:35 +0000

    Updated Preprocessing

--- tools.py
+++ tools.py
@@ -22,31 +22,149 @@ def download_genome(genome_dir, ref='hg19'):
     """
     # TODO: find the hg19 genome???
 
-def preprocess_fastq(fastq1, fastq2, output_dir, picard_dir, **params):
+def preprocess_fastq(fastq1, fastq2, output_dir, **params):
     """
     Performs all the steps before running the alignment. Temporary files
     saved in output_dir.
     """
-    picard_path = os.path.join(picard_dir, 'picard.jar')
-    # calls picard to convert fastq to an unaligned bam file
-    rc = subprocess.call('java -jar {3} FastqToSam F1={0} F2={1} O={2}/single_cells.bam SM=1'.format(fastq1, fastq2, output_dir, picard_path),
-            shell=True)
-    if rc == 1:
-        return 'failed: step 1'
-    # copy header - trim read 2
-    rc = subprocess.call('samtools view -H {0}/single_cells.bam > {0}/single_cells.trimmed.sam'.format(output_dir),
-            shell=True)
-    if rc == 1:
-        return 'failed: step 1'
-    # Trim read2 to UMI_BC3_BC2_BC1 if bases 81-86=="CATTCG" (no dephasing):
-    # TODO: should the awk script be modifiable?
-    rc = subprocess.call("""
-            samtools view {0}/single_cells.bam | awk 'NR%2==1{a=$0}NR%2==0 && substr($10,81,6)=="CATTCG"{print a"\n"$1"\t"$2"\t"$3"\t"$4"\t"$5"\t"$6"\t"$7"\t"$8"\t"$9"\t"substr($10,1,18)substr($10,49,8)substr($10,87,8)"\t"substr($11,1,18)substr($11,49,8)substr($11,87,8)"\t"$12}' >> {0}/single_cells.trimmed.sam
-            """.format(output_dir), shell=True)
-    # TODO: filter based on quality scores in barcode and UMI - allow only 1 base with phred<20 in each of BC and UMI.
-    # Tag headers with BC3_BC2_BC1_UMI
-    # Make reads unmated:
-    # Convert sam to fastq
+    
+    bases = list('ACGT')
+    def convert_degen_seq_to_list(seq):
+        """Uses recursion to convert a degenerate sequence to a list
+        For example: AGGN -> [AGGA, AGGC, AGGG, AGGT]"""
+        
+        seq_list = []
+        N_pos = seq.find('N')
+        if N_pos>=0:
+            for b in bases:
+                seq_list += convert_degen_seq_to_list(seq[:N_pos] + b + seq[N_pos+1:])
+        else:
+            seq_list.append(seq)
+        return seq_list
+
+    def editd1(seq):
+        """Returns the list of sequences with edit distance 1
+        It returns a sequence with the same length. So a base insertion
+        will result in the last base being truncated"""
+        
+        seq_len = len(seq)
+        
+        # Original sequence
+        edit_seqs = [seq]
+        
+        # Insertions
+        for i in range(seq_len):
+            for b in bases:
+                edit_seqs.append(seq[:i] +b + seq[i:-1])
+                
+        # Deletions
+        for i in range(seq_len):
+            edit_seqs.append(seq[:i] + seq[i+1:]+'-')
+        
+        # Nt changes
+        for i in range(seq_len):
+            for b in bases:
+                if b!=seq[i]:
+                    edit_seqs.append(seq[:i]+b+seq[i+1:])
+        
+        # 1nt shift forward
+        edit_seqs.append('-'+seq[:-1])
+        
+        # 1nt shift backward
+        edit_seqs.append(seq[1:]+'-')
+        
+        # Convert Ns to sequences
+        output_edit_seqs = []
+        for s in edit_seqs:
+            output_edit_seqs += convert_degen_seq_to_list(s)
+        
+        return output_edit_seqs
+
+    def bc_editd1_correction(barcodes,bc_pre='',bc_suf='', bc_start=None, bc_end=None):
+        pre_len = len(bc_pre)
+        bc_len = len(barcodes[0])
+        suf_len = len(bc_suf)
+        full_seq_len = pre_len + bc_len + suf_len
+        if bc_start is None:
+            bc_start = (pre_len-1)
+        if bc_end is None:
+            bc_end = pre_len + bc_len + 1
+
+        bc_dict = defaultdict(list)
+        for bc in barcodes:
+            seqs_d1 = unique([s[bc_start:bc_end] for s in unique(editd1(bc_pre + bc + bc_suf))])
+            [bc_dict[s].append(bc) for s in seqs_d1]
+        bc_map = pd.Series(bc_dict)
+        bc_map = bc_map[bc_map.apply(len)==1].apply(lambda s:s[0]).to_dict()
+        return bc_map
+
+    def fix_bc(bc,bc_map):
+        try:
+            bc_fixed = bc_map[bc]
+        except:
+            bc_fixed = ''
+        return bc_fixed
+
+    # Amplicon sequence
+    amp_seq = 'NNNNNNNNNNIIIIIIIIGTGGCCGATGTTTCGCATCGGCGTACGACTIIIIIIIIATCCACGTGCTTGAGAGGCCAGAGCATTCGIIIIIIII'
+
+    # Get location of cell barcodes in amplicon:
+    bc_len = 8
+    bc_starts = []
+    c = 0
+    while True:
+        bc_loc = amp_seq[c:].find('IIIIIIII')
+        if bc_loc==-1:
+            break
+        bc_starts.append(bc_loc + c)
+        c = bc_starts[-1] + bc_len
+
+    # Generate bc_map dictionary for each cell barcode. 
+    bc3_pre = amp_seq[bc_starts[0]-2:bc_starts[0]]
+    bc3_suf = amp_seq[bc_starts[0]+8:bc_starts[0]+10]
+    bc3_map = bc_editd1_correction(bc_8nt.values,
+                                   bc_pre=bc3_pre,
+                                   bc_suf=bc3_suf)
+
+    bc2_pre = amp_seq[bc_starts[1]-2:bc_starts[1]]
+    bc2_suf = amp_seq[bc_starts[1]+8:bc_starts[1]+10]
+    bc2_map = bc_editd1_correction(bc_8nt.values,
+                                   bc_pre=bc2_pre,
+                                   bc_suf=bc2_suf)
+    bc1_pre = amp_seq[bc_starts[2]-2:bc_starts[2]]
+    bc1_suf = 'N'
+    bc1_map = bc_editd1_correction(bc_8nt.values,
+                                   bc_pre=bc1_pre,
+                                   bc_suf=bc1_suf,
+                                   bc_start=1,
+                                   bc_end=10)
+
+    with gzip.open(fastq1,'rb') as f1, gzip.open(fastq2,'rb') as f2, open(output_dir + 'read1_cellbarcode_headers.fastq','w') as fout:
+        while True:
+            header2 = f2.readline()
+            if len(header2)==0:
+                break
+            seq2 = f2.readline().decode("utf-8")
+            bc1 = fix_bc(seq2[86-1:94],bc1_map)
+            bc2 = fix_bc(seq2[48-1:56+1],bc2_map)
+            bc3 = fix_bc(seq2[10-1:18+1],bc3_map)
+            umi = seq[:10]
+            strand2 = f2.readline()
+            qual2 = f2.readline()
+            
+            cellbc_umi = bc3 + bc2 + bc1 +'_' + umi
+            header1 = f1.readline().decode("utf-8")
+            seq1 = f1.readline().decode("utf-8")
+            strand1 = f1.readline().decode("utf-8")
+            qual1 = f1.readline().decode("utf-8")
+            
+            if len(cellbc_umi)==35:
+                header1 = '@' + bc3 + bc2 + bc1 +'_' + umi + '_' + qual2.decode("utf-8")[:10] + '_' + header1[1:]
+                fout.write(header1)
+                fout.write(seq1)
+                fout.write(strand1)
+                fout.write(qual1)
+
     return 0
 
 def run_star(genome_dir, output_dir):
